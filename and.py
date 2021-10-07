76729357ffa02be6133d060d9be5571850709c0f
rahulp-at-609438676299 <rahupaygude8293@gmail.com>
2021-10-07 12:17:17 +0530

    first commit

---
+++ and.py
@@ -0,0 +1,27 @@
+from utils.model import Perceptron
+from utils.all_utils import prepare_data,save_model
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import joblib
+from matplotlib.colors import ListedColormap
+
+AND = {
+    'x1':[0,0,1,1],
+    'x2':[0,1,0,1],
+    'y':[0,0,0,1]    
+}
+
+df = pd.DataFrame(AND)
+df
+
+X,y = prepare_data(df)
+ETA = 0.3  #Normaly bet 0 and 1
+EPOCHS = 10
+
+model = Perceptron(eta=ETA, epochs=EPOCHS)
+model.fit(X,y)
+
+_ = model.total_loss()
+
+save_model(model,'and.model')
---
+++ utils/model.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+class Perceptron:
+    def __init__(self,eta,epochs):
+        self.weights = np.random.rand(3) * 1e-4
+        print(f"initial weights before training:\n{self.weights}")
+        self.eta = eta # Learning Rate
+        self.epochs = epochs
+        
+    def ActivationFunction(self,inputs,weights):
+        z = np.dot(inputs,weights)   # z = w*x
+        return np.where(z > 0, 1, 0)
+        
+    def fit(self,X,y):
+        self.X = X
+        self.y = y
+        
+        X_with_bias = np.c_[self.X, -np.ones((len(self.X),1))]   #for concatenation
+        print(f"X_with_bias:\n{X_with_bias}")
+        
+        for epoch in range(self.epochs):
+            print('--'*10)
+            print(f'for epoch:{epoch}')
+            print('--'*10)
+            
+            y_hat = self.ActivationFunction(X_with_bias,self.weights) #forward propogation
+            print(f'Predicted value after forward pass:\n {y_hat}')
+            self.error = self.y - y_hat
+            print(f'error:\n {self.error}')
+            self.weights = self.weights + self.eta * np.dot(X_with_bias.T , self.error) #backword Prop
+            print(f'updated weights after epoch:\n {epoch}/{self.epochs} : \n{self.weights}')
+            print('####'*10)
+            
+    def predict(self,X):
+        X_with_bias = np.c_[X, -np.ones((len(X),1))]
+        return self.ActivationFunction(X_with_bias, self.weights)
+    
+    def total_loss(self):
+        total_loss = np.sum(self.error)
+        print(f'total loss:{total_loss}')
+        return total_loss
+    
